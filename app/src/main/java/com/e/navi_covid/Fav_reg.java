fc9f14846c3cc076331b04286ade0c82c2ffa65f
アルミス <altimis453@gmail.com>
2021-07-29 14:31:13 +0900

    登録画面の追加実装と、電話ボタンの実装

---
+++ app/src/main/java/com/e/navi_covid/Fav_reg.java
@@ -0,0 +1,14 @@
+package com.e.navi_covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fav_reg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fav_reg);
+    }
+}
